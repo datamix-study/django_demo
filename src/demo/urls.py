9341276abe0267a38dfe83281114d3fe38d568ff
snufkin92 <shiroshi92@gmail.com>
2019-10-14 17:09:10 +0900

    add:新規プロジェクト追加

--- src/demo/urls.py
+++ src/demo/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # login
     path('', views.index, name='index'),
+
+    # top
+    path('', views.top, name='top'),
 ]
--- src/demo/views.py
+++ src/demo/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'demo/login.html')
+
+def top(request):
+    return HttpResponse("this is top.")
