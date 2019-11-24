b2e2ad534066266a7bdb13ea7f93c7152d62e236
snufkin92 <shiroshi92@gmail.com>
2019-11-24 16:27:59 +0900

    modify:商品モデルの追加
    ・商品モデルの追加
    ・管理画面から編集可能となるようにadmin.pyを設定
    ・動作確認用のデータ追加

--- src/demo/admin.py
+++ src/demo/admin.py
@@ -2,5 +2,7 @@
 from django.contrib import admin
 
 from .models import User
+from .models import Item
 
 admin.site.register(User)
+admin.site.register(Item)
--- src/demo/models.py
+++ src/demo/models.py
@@ -1,10 +1,43 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
+    """
+    ログインユーザ
+    """
+    # ユーザ名
     username = models.CharField(max_length=20)
+
+    # パスワード
     password = models.CharField(max_length=20)
+
+    # 登録日付
     regist_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.username;
+
+
+class Item(models.Model):
+    """
+    商品
+    """
+    # 商品名
+    name = models.CharField(max_length=255)
+
+    # 商品画像ファイルパス
+    image_path = models.CharField(max_length=20)
+
+    # 商品説明
+    descrition = models.CharField(max_length=3000)
+
+    # 価格
+    price = models.PositiveSmallIntegerField()
+
+    # 在庫数
+    stock_quantity = models.PositiveSmallIntegerField()
+
+
+# class Cart(models.Model):
+#     """
+#     カート情報
+#     """
